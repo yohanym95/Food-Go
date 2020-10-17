d459ba6bd3c5724b66aca4a30526b5143aa552bb
YohanM95 <yohanm@99x.lk>
2020-10-17 18:50:35 +0530

    Create basic component
    create bottom navigator & basic widgets

--- lib/main.dart
+++ lib/main.dart
@@ -32,10 +32,60 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xFFFCFCFC),
       appBar: AppBar(
-        title: Text(widget.title),
+        backgroundColor: Color(0xFFfcfcfc),
+        title: Text(
+          'Food-Go',
+          style: TextStyle(
+            color: Color(0xFFfc6a26),
+          ),
+        ),
+        centerTitle: true,
+        elevation: 0.0,
+        leading: IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.menu,
+              color: Colors.black,
+            )),
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(
+                Icons.shopping_cart,
+                color: Colors.black,
+              ),
+              onPressed: () {})
+        ],
       ),
-      body: Center(),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text("Let's Eat \nOrder Your Food Now",
+            style: TextStyle(
+              fontSize: 28.0,
+              fontWeight: FontWeight.w700
+            ),)
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: Color(0xFFfcfcfc),
+          elevation: 3.0,
+          unselectedItemColor: Colors.black,
+          selectedItemColor: Color(0xFFfc6a26),
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.home), title: Text('Home')),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.store), title: Text('Our Shop')),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.home), title: Text('Favourite')),
+            BottomNavigationBarItem(icon: Icon(Icons.home), title: Text('Home'))
+          ]),
     );
   }
 }
